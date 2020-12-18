2e0ab99e09ecce561bbf80c0ce419b31b931f678
Mystic Kitty <djabranton@gmail.com>
2020-12-18 12:43:37 -0800

    Set up basic associates. Need foreign key references now.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag
+  // as: 'voted_posts',
+  // foreignKey: 'user_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+  // as: 'voted_posts',
+  // foreignKey: 'user_id'
+});
+
+
 
 module.exports = {
   Product,
