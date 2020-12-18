24b15e90b4df22e4115151c0d547b5b295b54989
Mystic Kitty <djabranton@gmail.com>
2020-12-18 15:16:40 -0800

    Associates done.

--- models/index.js
+++ models/index.js
@@ -5,20 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
-  // as: 'voted_posts',
-  // foreignKey: 'user_id'
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: ' product_id'
 });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
-  // as: 'voted_posts',
-  // foreignKey: 'user_id'
+  through: ProductTag,
+  as: 'tag_products',
+  foreignKey: ' tag_id'
 });
 
 
